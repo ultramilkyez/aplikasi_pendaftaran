643f943b6ab633046e3f815d09aa051037baca83
Ultramilkyez_ <124322443+ultramilkyez@users.noreply.github.com>
2023-02-04 08:13:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<?php
+
+	include "koneksi.php";
+?>
+<head>
+	<title>BERANDA</title>
+</head>
+<body style= "background-image: url(kertas.jpg); background-size: cover;">
+
+	<p style="color : pink;"><b>Selamat Datang di Acara Gebyar | SMK LUKMAN AL HAKIM KUDUS</b>
+	<hr>
+	<p style="color : pink;"> Acara ini ialah untuk mengasah bakat anak muda yang terdiri:</p>
+	<p style="color : pink;">- Lomba MPL ID</p>
+	<p style="color : pink;">- Lomba FF ID</p>
+	<p style="color : pink;">- Lomba E-Sport</p>
+	<p style="color : pink;">- Lomba Fotografi & Videografi</p>
+	<p style="color : pink;">- DLL</p>
+	<p style="color : pink;">Acara ini Dimeriahkan Oleh Jihan Audy, Denny Caknan, Woro widowati dan masih banyak artis terkenal lainnya </p>
+	<p style="color : pink;">Jika Kalian ingin/minat bisa mendaftar di kolom bawah tersebut</p>
+	<p>
+	<a href="pendaftaran.php"><input type="submit" value="Daftar"></a>
+	<a href="login.php"><input type="submit" value="Login Admin"></a>
+	</p>
+	<hr>
+	<div style="color : pink;">Copyright &copy; 2023 | SMK Luqman Al Hakim Kudus</div>
+</body>
